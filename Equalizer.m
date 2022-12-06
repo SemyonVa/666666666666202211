9044586cfb242d77b272ba3baaa221286be7b13c
Семён Васильев <90034714+SemyonVa@users.noreply.github.com>
2022-12-06 17:32:56 +0300

    Add files via upload

--- Equalizer.m
+++ Equalizer.m
@@ -1,58 +1,58 @@
 classdef Equalizer < handle 
-properties (Constant)
-    freqArray=[31,62,125,250,500,1000,2000,4000,8000,16000]
-end
-properties(Access=public)
-    gain=[1,1,1,1,1,1,1,1,1,1]
-end
-properties(Access=protected)
-    bBank=[]
-    initB=[]
-end
-properties(GetAccess=public, SetAccess=protected)
-    order=64
-    fs=44100
-end
-methods
-    function obj = Equalizer(order,fs)
-        if nargin == 0
-            order = 64;
-            fs = 44100;
-        end
-        obj.order=order;
-        obj.fs=fs;
+    properties(Constant)
+        freqArray=[31,62,125,250,500,1000,2000,4000,8000,16000]
     end
-    function[signalOut,initB]=filtering(obj,signalBank,gain,initB)
-        b=sum(gain.*bBank,1);
-        [signalOut,initB]=filter(b,1,signal,initB);
+    properties(Access=public)
+        gain=[1,1,1,1,1,1,1,1,1,1]
     end
-    function bBank = CreatingFilters(freqArray,order,fs)
-        freqArrayNorm=freqArray/(fs/2);
-        bBank=zeros(length(freqArray),order+1);
-        for i = 1:length(freqArray)
-            if i==1
-                mLow=[1,1,0,0];
-                freqLow=[0,freqArrayNorm(1),2*freqArrayNorm(1),1];
-                bBank(1,:)=fir2(order,freqLow,mLow);
-            elseif i== length(freqArray)
-                mHigh=[0,0,1,1];
-                freqHigh=[0,freqArrayNorm(end)/2,freqArrayNorm(end),1];
-                bBank(length(freqArray),:),fir2(ordrer,freqHigh,mHigh);
-            else
-                mBand=[0,0,1,0,0];
-                freqBand=[0,freqArrayNorm(i-1),freqArrayNorm(i),freqArrayNorm(i+1),1];
-                bBank(i,:)=fir2(order,freqBand,mBand)
+    properties(Access=protected)
+        bBank=[]
+        initB=[]
+    end
+    properties(GetAccess=public,SetAccess=protected)
+        fS=44100
+        order=64
+    end
+    methods
+        function obj = Equalizer(order,fS)
+            if nargin == 0
+                order = 64;
+                fS = 44100;
             end
+            obj.order=order;
+            obj.fS=fS;
+            CreatingFilters();
         end
-    end
-    function[HdB,w]=GetFreqResponse(bBank,order,fs,gain)
-        b=sum(gain.*bBank)
-        for i=1:10
-            [h(i,:),w(i,:)]=freqz(b(i,:),1,order);
+        function[signalOut,initB]=Filtering(obj)
+            b=sum(obj.gain.*obj.bBank,1);
+            [signalOut,initB]=filter(b,1,signal,initB);
         end
-        xdb=@(x)20*log10(x);
-        Hdb(abs(H));
-        w=(w/pi)*(fs/2);
+       function CreatingFilters(obj)
+            freqArrayNorm = obj.freqArray/(obj.fS/2);
+                for i = 1:length(obj.freqArray)
+                    if i==1
+                        mLow = [1, 1, 0, 0];
+                        freqLow = [0, freqArrayNorm(1), 2*freqArrayNorm(1), 1];
+                        obj.bBank(1, :) = fir2(obj.order, freqLow, mLow);
+                    elseif i == length(obj.freqArray)
+                        mHigh = [0, 0, 1, 1];
+                        freqHigh = [0, freqArrayNorm(end)/2, freqArrayNorm(end),1];
+                        obj.bBank(length(obj.freqArray),:) = fir2(obj.order, freqHigh, mHigh);
+                    else
+                        mBand = [0, 0, 1, 0, 0];
+                        freqBand = [0, freqArrayNorm(i-1), freqArrayNorm(i), freqArrayNorm(i+1), 1];
+                        obj.bBank(i, :) = fir2(obj.order, freqBand, mBand);
+                    end
+            end
+       end
+       function [Hdb,w]=GetFreqResponse(obj)
+           b=sum(obj.gain.*obj.bBank);
+           for i=1:10
+               [H(i,:),w(i,:)]=freqz(b(i,:),1,obj.order);
+           end
+           xdb=@(x)20*log10(x); 
+           Hdb=xdb(abs(H));
+           w=(w/pi)*(obj.fS/2);
+       end
     end
-end 
 end
